3b3e6b9bf031e6a23577e77e5a8ef81a27abaf18
DealiAxy <dealiaxy@gmail.com>
2020-06-19 01:01:14 +0800

    搞了一堆花里胡哨的，结果也没把Tab做好
    Signed-off-by: DealiAxy <dealiaxy@gmail.com>

--- app/src/main/java/cn/deali/poemnote/fragment/HomeFragment.kt
+++ app/src/main/java/cn/deali/poemnote/fragment/HomeFragment.kt
@@ -5,17 +5,56 @@ import android.view.LayoutInflater
 import android.view.View
 import cn.deali.poemnote.R
 import com.qmuiteam.qmui.arch.QMUIFragment
-import com.qmuiteam.qmui.widget.QMUITopBar
-import com.qmuiteam.qmui.widget.QMUITopBarLayout
+import com.qmuiteam.qmui.util.QMUIDisplayHelper
+import cn.deali.poemnote.ext.toast
+import com.qmuiteam.qmui.widget.tab.QMUITabSegment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : QMUIFragment() {
     @SuppressLint("InflateParams")
     override fun onCreateView(): View {
-        val view = LayoutInflater.from(activity).inflate(R.layout.fragment_home, null)
+        return LayoutInflater.from(activity).inflate(R.layout.fragment_home, null)
+    }
 
-        val topBar: QMUITopBarLayout = view.findViewById(R.id.topbar)
-        topBar.setTitle("诗词笔记")
+    override fun onViewCreated(rootView: View) {
+        super.onViewCreated(rootView)
 
-        return view
+        topbar.setTitle("诗词笔记")
+
+        initTabs()
+    }
+
+
+    private fun initTabs() {
+        context?.toast("初始化")
+
+        val builder = tabs.tabBuilder()
+        builder.setSelectedIconScale(1.2f)
+            .setTextSize(QMUIDisplayHelper.sp2px(context, 13), QMUIDisplayHelper.sp2px(context, 15))
+            .setDynamicChangeIconColor(false)
+
+        val component = builder
+            .setNormalDrawable(context?.getDrawable(R.mipmap.icon_tabbar_component))
+            .setSelectedDrawable(context?.getDrawable(R.mipmap.icon_tabbar_component_selected))
+            .setText("组件")
+            .build(context)
+
+        val lab = builder
+            .setNormalDrawable(context?.getDrawable(R.mipmap.icon_tabbar_lab))
+            .setSelectedDrawable(context?.getDrawable(R.mipmap.icon_tabbar_lab_selected))
+            .setText("实验室")
+            .build(context)
+
+        val utils = builder
+            .setNormalDrawable(context?.getDrawable(R.mipmap.icon_tabbar_util))
+            .setSelectedDrawable(context?.getDrawable(R.mipmap.icon_tabbar_util_selected))
+            .setText("工具")
+            .build(context)
+
+        tabs.addTab(component)
+            .addTab(lab)
+            .addTab(utils)
+
+        tabs.mode = QMUITabSegment.MODE_FIXED
     }
 }
