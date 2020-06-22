813d79b7565100eff4ed6b3bb804aa77bc45327d
DealiAxy <dealiaxy@gmail.com>
2020-06-22 17:45:44 +0800

    改了一丢丢东西
    Signed-off-by: olap <dealiaxy@gmail.com>

--- app/src/main/java/cn/deali/poemnote/ext/toast.kt
+++ app/src/main/java/cn/deali/poemnote/ext/toast.kt
@@ -6,3 +6,7 @@ import android.widget.Toast
 fun Context.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, length).show()
 }
+
+fun Context.longToast(message: String, length: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(this, message, length).show()
+}
